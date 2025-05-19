b3864596d0ea9c1b26fc2f28d52ec2dbd35e157b
Adam Burucs <adam.burucs@trendency.hu>
2025-05-19 11:10:57 +0200

    feat(client): add tailwind and shadcnui

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,20 +1,35 @@
 /**
  * Copyright (c) 2025-present Adam Burucs
- * 
+ *
  * This software is released under the MIT License.
  * https://opensource.org/licenses/MIT
  */
 
-import './App.css'
+import './App.css';
+import Employee from './components/Employee';
 
 function App() {
   return (
     <>
-      <h1>Skill Matrix App</h1>
-      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sequi similique sunt accusamus consequatur vel, aliquam nemo optio voluptates quae, itaque, dolorum maxime aliquid! Recusandae quasi porro accusantium, molestias a dolore?
+      <h1 className="text-3xl font-bold underline">Skill Matrix App</h1>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sequi
+        similique sunt accusamus consequatur vel, aliquam nemo optio voluptates
+        quae, itaque, dolorum maxime aliquid! Recusandae quasi porro
+        accusantium, molestias a dolore?
       </p>
+      <div className="grid gap-4">
+        <Employee />
+        <Employee />
+        <Employee />
+        <Employee />
+        <Employee />
+        <Employee />
+        <Employee />
+        <Employee />
+      </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,11 +1,18 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import tailwindcss from '@tailwindcss/vite';
+import path from 'node:path';
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), tailwindcss()],
   preview: {
     host: '0.0.0.0',
     port: 3000,
+  },
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+    },
   },
 });
