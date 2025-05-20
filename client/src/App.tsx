2e6caf49836a3cddc978820b3c12822baa9efdb8
Adam Burucs <adam.burucs@trendency.hu>
2025-05-20 10:46:18 +0200

    feat(client): handle error and loading status display

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -36,6 +36,15 @@ function App() {
     }
   };
 
+  type Message = typeof error | typeof loading;
+
+  function displayErrorOrLoading(message: Message): Message {
+    if (message === false || message === '' || message === null || message === undefined) {
+      return '🟢 no (none)';
+    }
+    return message;
+  }
+
   useEffect(() => {
     fetchEmployees();
   }, []);
@@ -48,8 +57,8 @@ function App() {
       <main className="my-4">
         <p className="mb-4">You can view the employees' skill matrix here.</p>
         <div className="mb-4"><Button onClick={fetchEmployees}>Refresh</Button></div>
-        <div>Loading: {loading}</div>
-        <div className="mb-4">Error: {error?.toString()}</div>
+        <div>Loading: {displayErrorOrLoading(loading)?.toString()}</div>
+        <div className="mb-4">Error: {displayErrorOrLoading(error)?.toString()}</div>
         <EmployeeList employees={employees} />
       </main>
       <footer className="">Made with ❤️ love and ☕ coffee. Since 2025.</footer>
