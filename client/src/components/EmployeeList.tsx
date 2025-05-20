52a07f15ef7c2a00c07eaaeb1e55ea7936ebcc93
Adam Burucs <adam.burucs@trendency.hu>
2025-05-20 10:45:50 +0200

    feat(client): add copyright text

--- client/src/components/EmployeeList.tsx
+++ client/src/components/EmployeeList.tsx
@@ -1,3 +1,10 @@
+/**
+ * Copyright (c) 2025-present Adam Burucs
+ *
+ * This software is released under the MIT License.
+ * https://opensource.org/licenses/MIT
+ */
+
 import { Employee } from '@/types/types';
 import EmployeeCard from './EmployeeCard';
 
