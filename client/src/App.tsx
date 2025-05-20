34d8bca0df725db21be23bf93e7b12698f1d110b
Adam Burucs <adam.burucs@trendency.hu>
2025-05-20 14:25:14 +0200

    feat(client): format file, correct json import

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -8,9 +8,9 @@
 import { useEffect, useState } from 'react';
 import './App.css';
 import EmployeeList from './components/EmployeeList';
-import employeesList from '@/constants/employees.json' with { type: 'json' };
+import employeesList from '@/constants/employees.json';
 import { Employee } from './types/types';
-import { Button } from "@/components/ui/button"
+import { Button } from '@/components/ui/button';
 
 function App() {
   const [employees, setEmployees] = useState<Employee[]>(employeesList);
@@ -21,14 +21,16 @@ function App() {
     setLoading(true);
     setError(null);
     try {
-      const response = await fetch(`${import.meta.env.VITE_API_URL}/api/employees`);
+      const response = await fetch(
+        `${import.meta.env.VITE_API_URL}/api/employees`,
+      );
       if (!response.ok) {
         throw new Error(`HTTP error! status: ${response.status}`);
       }
       const data = await response.json();
       setEmployees(data);
     } catch (err: unknown) {
-      if (typeof err === "object" || typeof err === "string") {
+      if (typeof err === 'object' || typeof err === 'string') {
         setError(err || 'Failed to fetch employees');
       }
     } finally {
@@ -39,7 +41,12 @@ function App() {
   type Message = typeof error | typeof loading;
 
   function displayErrorOrLoading(message: Message): Message {
-    if (message === false || message === '' || message === null || message === undefined) {
+    if (
+      message === false ||
+      message === '' ||
+      message === null ||
+      message === undefined
+    ) {
       return '🟢 no (none)';
     }
     return message;
@@ -56,9 +63,13 @@ function App() {
       </header>
       <main className="my-4">
         <p className="mb-4">You can view the employees' skill matrix here.</p>
-        <div className="mb-4"><Button onClick={fetchEmployees}>Refresh</Button></div>
+        <div className="mb-4">
+          <Button onClick={fetchEmployees}>Refresh</Button>
+        </div>
         <div>Loading: {displayErrorOrLoading(loading)?.toString()}</div>
-        <div className="mb-4">Error: {displayErrorOrLoading(error)?.toString()}</div>
+        <div className="mb-4">
+          Error: {displayErrorOrLoading(error)?.toString()}
+        </div>
         <EmployeeList employees={employees} />
       </main>
       <footer className="">Made with ❤️ love and ☕ coffee. Since 2025.</footer>
